83588113e7cb3f9f41a442be01a3badd7dc41653
Ji Sang Seo <inaki96@naver.com>
2023-03-15 13:52:06 +0900

    feat: post processings

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import * as THREE from 'three';
 import { Mesh } from 'three';
 import { useGLTF, Environment } from '@react-three/drei';
 import { GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
+import { EffectComposer, DepthOfField } from '@react-three/postprocessing';
 
 type GLTFBanana = GLTF & {
   nodes: {
@@ -28,13 +29,15 @@ type GLTFApple = GLTF & {
 
 function Banana({ z }: { z: number }) {
   const ref = useRef<Mesh>(null);
-  const [clicked, setClicked] = useState<boolean>(false);
   const { viewport, camera } = useThree();
   const { width, height } = viewport.getCurrentViewport(camera, [0, 0, z]);
   const [data] = useState({
     x: THREE.MathUtils.randFloatSpread(1),
     y: THREE.MathUtils.randFloatSpread(height),
-    z
+    z,
+    rx: Math.random() * Math.PI,
+    ry: Math.random() * Math.PI,
+    rz: Math.random() * Math.PI
   });
   const [idx] = useState(Math.round(Math.random()));
   const { nodes: bananaNodes, materials: bananaMaterials } = useGLTF('/banana-v1-transformed.glb') as GLTFBanana;
@@ -50,15 +53,15 @@ function Banana({ z }: { z: number }) {
     {
       geometry: appleNodes['apple_low_obj_Material_#35_0'].geometry,
       material: appleMaterials.Material_35,
-      ['material-emissive']: 'red',
+      ['material-emissive']: 'rgb(199,3,0)',
       rotation: new THREE.Euler(-0.05, 0.04, -0.01),
-      scale: 0.11
+      scale: 0.04
     }
   ];
 
   useFrame((state) => {
-    const { elapsedTime } = state.clock;
-    ref.current!.position.set(data.x * width, (data.y += 0.3), data.z);
+    ref.current!.rotation.set((data.rx += 0.001), (data.ry += 0.004), (data.rz += 0.0005));
+    ref.current!.position.set(data.x * width, (data.y += 0.05), data.z);
     if (data.y > height / 1.5) {
       data.y = -height / 1.5;
     }
@@ -79,15 +82,19 @@ function Banana({ z }: { z: number }) {
 useGLTF.preload('/banana-v1-transformed.glb');
 useGLTF.preload('/apple-v1-transformed.glb');
 
-function App({ count = 30 }) {
+function App({ count = 100, depth = 80 }) {
   return (
-    <Canvas>
-      <ambientLight intensity={0.2} />
-      <spotLight position={[10, 10, 10]} intensity={2} />
+    <Canvas gl={{ alpha: false }} camera={{ near: 0.01, far: 110, fov: 30 }}>
+      <color attach="background" args={['#ffbf40']} />
+
+      <spotLight position={[10, 10, 10]} intensity={1} />
       <Suspense fallback={null}>
         {Array.from({ length: count }).map((_, idx) => (
-          <Banana key={`box ${idx}`} z={-idx} />
+          <Banana key={`box ${idx}`} z={-(idx / count) * depth - 20} />
         ))}
+        <EffectComposer>
+          <DepthOfField target={[0, 0, depth / 2]} focalLength={0.5} bokehScale={11} height={700} />
+        </EffectComposer>
         <Environment preset={'sunset'} />
       </Suspense>
     </Canvas>
